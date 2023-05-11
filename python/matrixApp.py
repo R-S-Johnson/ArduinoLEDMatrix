a15931b89731130e12b33a8c94bd63e3a5f6eb08
Riley Johnson <riley.scott.johnson@gmail.com>
2023-05-11 17:27:24 -0400

    new output format, remove redundencies

--- python/matrixApp.py
+++ python/matrixApp.py
@@ -11,23 +11,30 @@ class MatrixUI:
         for i in range(self.rows):
             row = []
             for j in range(self.cols):
-                button = tk.Button(self.master, command=lambda i=i, j=j: self.on_button_click(i, j), width=5, bg="white")
+                button = tk.Button(self.master, command=lambda i=i, j=j: self.onButtonClick(i, j), width=5, bg="#000000")
                 button.grid(row=i, column=j)
                 row.append(button)
             self.buttons.append(row)
             saveButton = tk.Button(self.master, command=self.saveButtonPress, text="save")
             saveButton.grid(row=self.rows, column=self.cols-1)
 
-    def on_button_click(self, i, j):
+    def onButtonClick(self, i, j):
         _, color = colorchooser.askcolor()
         if color:
             self.buttons[i][j].config(bg=color)
 
     def saveButtonPress(self):
         f = open("color.txt", 'w')
-        for row in self.buttons:
-            for button in row:
-                f.write(str(button.cget('bg')) + "\n")
+        for rowi in range(len(self.buttons)):
+            for colj in range(len(self.buttons[rowi])):
+                color = self.buttons[rowi][colj].cget("bg")
+                if color != "#000000":
+                    colorRGB = tuple(int(color.lstrip("#")[i:i+2], 16) for i in (0, 2, 4))
+                    f.write(str(rowi) + " "
+                            + str(colj) + " "
+                            + str(colorRGB[0]) + " "
+                            + str(colorRGB[1]) + " "
+                            + str(colorRGB[2]) + "\n")
         f.close()
         self.master.destroy()
 
